d8570f799860dac0ec16c065f2a6888793442425
Walidoux <ma.walidkorchi@icloud.com>
2023-04-19 12:26:18 +0000

    refactor(index): clean up code

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,29 @@
-import { readFile } from 'fs'
-import { tags, Tag } from 'swf-types'
+import { readFile } from 'node:fs'
+
+import type { tags } from 'swf-types'
 
 readFile(process.cwd() + '/swfs/bunny.swf', async (_, data) => {
-  // Define type compression from reading the three first bytes of the file
-  // defineTypeCompression(data)
-
   const SWF = (await import('swf-parser')).parseSwf(data)
 
   const images = SWF.tags.filter((tag) => {
     const mediaType = (tag as tags.DefineBitmap).mediaType
     return String(mediaType).startsWith('image')
-  }) as Extract<Tag, tags.DefineBitmap>[]
+  }) as tags.DefineBitmap[]
 
-  const className = (SWF.tags.filter((tag) => (tag as tags.PlaceObject).name)[0] as tags.PlaceObject).name
-  const current = images.map((img) => ({
-    path: className as string,
+  const className = (
+    SWF.tags.filter((tag) => {
+      return (tag as tags.PlaceObject).name
+    })[0] as tags.PlaceObject
+  ).name
 
-    // Convertion from Uint8Array to Buffer: Buffer.from(img.data)
-    // we need to find a way for reading this buffer, can it be read in base64 ?
-    contents: img.data
-  }))
+  const current = images.map((img) => {
+    return {
+      path: className as string,
+      contents: img.data
+    }
+  })
 
   console.log(current)
 
-  // unhandled error: "free-tex-packer-core: Error reading, mine must be a string" ?
   // const packerAtlas = await packAsync(current)
 })
